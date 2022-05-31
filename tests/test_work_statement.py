8f294d59b9cc49aeb80c7426db42a578e3e8b119
Ariel Sperduti <arielbmx@gmail.com>
2022-05-30 22:59:49 -0300

    add the missing tests for the work_statement

--- tests/test_work_statement.py
+++ tests/test_work_statement.py
@@ -8,3 +8,15 @@ def test_work_statement():
     assert work_statement.day == "MO"
     assert work_statement.start_time == timedelta(hours=10, minutes=00)
     assert work_statement.end_time == timedelta(hours=11, minutes=00)
+
+
+def test_work_statement_get_cost():
+    work_statement = WorkStatement(day="MO", start_time="10:00", end_time="11:00")
+
+    assert work_statement.get_cost() == 15.0
+
+
+def test_work_statement_get_cost_multiple_rules():
+    work_statement = WorkStatement(day="MO", start_time="8:00", end_time="11:00")
+
+    assert work_statement.get_cost() == 55.0
